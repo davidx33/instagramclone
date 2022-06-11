4e16f5d481330f8a84278767083b44f6fe735409
David Xu <david.xu@duke.edu>
2022-06-11 10:00:19 -0400

    working on top navbar

--- components/Header.js
+++ components/Header.js
@@ -31,11 +31,13 @@ function Header() {
         />
         </div>
 
-        <div className='relative mt-11 p-3 rounded-md bg-red-500'>
+        <div className='relative mt-11 p-3 rounded-md'>
             <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
                 <SearchIcon className='h-5 w-5 text-gray-500'/>
             </div>
-           <input type="text" placeholder='Search'></input> 
+           <input className='bg-gray-50 block w-full pl-10 sm:text-sm 
+           border-gray-300 focus:ring-black focus:border-black' 
+           type="text" placeholder='Search'></input> 
         </div>
        
 
