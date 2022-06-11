9cc17f3bb6eb0672699ea5252502c5445802215e
David Xu <david.xu@duke.edu>
2022-06-11 15:40:49 -0400

    figured out how to do fakerjs

--- components/Stories.js
+++ components/Stories.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import faker from "faker"
+import { Faker, faker } from '@faker-js/faker';
 
 function Stories() {
 
-    React.useEffect(() => {
-        const suggestions = [...Array[20]].map((_, i) => ({
-            ...faker.helpers.contextualCard()
-        })
+React.useEffect(() => {
 
-        )
-    }, [])
+    const [suggestions, setSuggestions] = React.useState([])
+
+    const suggestions = [...Array(20)].map((_, i) => ({
+        username: faker.image.avatar(),
+        avatar: faker.internet.userName(),
+        id: i
+    }));
+}, [])
+
+setSuggestions(suggestions)
+
   return (
     <div>
+        {suggestions.map (profile => (
+        <Story key={profile.id} 
+            img = {profile.avatar} 
+            username = {profile.username}/>
+        ))}
         {/* use Fakerjs to input fake data */}
     </div>
   )
