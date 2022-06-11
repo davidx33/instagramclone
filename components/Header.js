1d7d2cc2a178238373acd3f9c7ee5bf937b53e41
David Xu <david.xu@duke.edu>
2022-06-11 10:24:59 -0400

    making edits

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,13 @@ import {
     PaperAirplaneIcon,
     MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+import Home from '../pages';
 
 function Header() {
   return (
     <div>
-        <div className= "flex justify-between max-w-6xl">
+        <div className= "flex justify-between max-w-6xl mx-5 xl:mx-auto">
         <div className='relative hidden lg:inline-grid w-24
         cursor-pointer'>
             <Image
@@ -31,23 +33,27 @@ function Header() {
         />
         </div>
 
-        <div className='relative mt-11 p-3 rounded-md'>
+        <div className='max-w-xs'>
+        <div className='relative mt-1 p-3 rounded-md'>
             <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
                 <SearchIcon className='h-5 w-5 text-gray-500'/>
             </div>
            <input className='bg-gray-50 block w-full pl-10 sm:text-sm 
-           border-gray-300 focus:ring-black focus:border-black' 
-           type="text" placeholder='Search'></input> 
+           border-gray-300 focus:ring-black focus:border-black rounded-md' 
+           type="text" placeholder='Search' />
         </div>
-       
-
-        {/* Right */}
-
         </div>
-       
-        {/* left will be logo, middle will be search, right will be permissions */}
+        <div className='flex items-center justify-end space-x-4'>
+        <HomeIcon className='h-10 w-10'/>
+        <MenuIcon className='h-6 md:hidden cursor-pointer'/>
+
+        <PaperAirplaneIcon className='h-10 w-10' />
+        </div>
+        </div>
     </div>
   );
 }
 
-export default Header
+// left, right and middle aspects of the top navbar/components
+
+export default Header 
