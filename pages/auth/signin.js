ca1c967c433a1649fc09b2c16d9cdb7e11ced835
David Xu <david.xu@duke.edu>
2022-06-12 14:06:41 -0400

    working on authentication

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,13 +1,30 @@
 import { async } from '@firebase/util'
+import { getProviders, signIn as SignIntoProvider } from "next-auth/react"
 import React from 'react'
 
-function signIn() {
-  return (
-    <div>I am the sign in page</div>
-  )
+function signIn({ providers }) {
+    return (
+    <>
+    {Object.values(providers).map((provider) => (
+      <div key={provider.name}>
+        <button onClick={() => SignIntoProvider(provider.id)}>
+          Sign in with {provider.name}
+        </button>
+      </div>
+    ))}
+  </>
+    )
 }
 // serverside rendering
 export async function getServerSideProps() {
-    
+    // gets the providers for google from nextauth
+    const providers = await getProviders();
+
+    return {
+        props: {
+            providers,
+        }
+    }
+
 }
 export default signIn
